d960da6d05186ddf50d3be13a35b68d42bc73376
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2023-10-14 14:37:15 -0700

    Fixed nebulous 'fa' icon 404 logging;

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,13 @@ export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
 
     css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+    build: {
+        transpile: [
+            '@fortawesome/vue-fontawesome',
+            '@fortawesome/fontawesome-svg-core',
+            '@fortawesome/free-solid-svg-icons',
+            '@fortawesome/free-regular-svg-icons',
+            '@fortawesome/free-brands-svg-icons',
+        ],
+    },
 });
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,15 +1,11 @@
-// For Nuxt 3
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 
-// This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
-// You can add your icons directly in this plugin. See other examples for how you
-// can add other styles or just individual icons.
 library.add(fab, fas, far);
 
 export default defineNuxtPlugin((nuxtApp) => {
