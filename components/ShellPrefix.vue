708f349d3ca1b926d03375fe8142c44ed4e594ba
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2023-10-30 17:13:26 -0700

    Changed all opacity to text opacity;

--- components/ShellPrefix.vue
+++ components/ShellPrefix.vue
@@ -1,9 +1,9 @@
 <template>
-  <span class="opacity-80">
+  <span class="text-red-500/80">
     <span class="d-user"></span>
     <span>@localhost: ~</span>
     <span class="d-path font-bold"></span>
-    <span class="mr-1.5">$</span> 
+    <span>$ </span> 
   </span>
 </template>
 
