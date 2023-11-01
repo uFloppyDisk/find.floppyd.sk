8d3a21945600e193f7cd40e01a262b08a2268d55
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2023-10-31 17:55:41 -0700

    Made group hover event more accurate;

--- components/DisplayLinks.vue
+++ components/DisplayLinks.vue
@@ -5,97 +5,99 @@
         'opacity-0 invisible': !showLinks,
     }"
   >
-  <div class="group flex flex-col-reverse grow gap-2">
-      <div class="md:hidden pt-6"></div>
-      <div
-        v-for="category in categories" 
-        :key="category[0]"
-        class="group/category flex flex-col grow py-1 pl-2 pr-1"
-        :class="{
-          'mb-4': category[0] === 'priority',
-          'backdrop-opacity-90 backdrop-saturate-200 backdrop-brightness-50 backdrop-blur-sm shadow-lg shadow-black/50': !collapseCategory[category[0]]
-        }"
-      >
-        <div 
-          class="flex cursor-pointer"
-          @click="collapse((category[0] as Categories))"
-        >
-          <div class="flex basis-4 shrink-0 opacity-50 justify-center items-center hover:opacity-100 transition-opacity">
-            <font-awesome-icon 
-              class="fa-1x transition-transform duration-100"
-              :class="{
-                '-rotate-90': collapseCategory[category[0]]
-              }"
-              :icon="[
-                'fas',
-                'chevron-down'
-              ]"
-            />
-          </div>
-          <div 
-            class="flex basis-full mx-2 border-dotted border-red-500 font-bold text-sm capitalize opacity-75 group-hover/category:opacity-100 transition-opacity"
-            :class="{
-              'border-b': !collapseCategory[category[0]]
-            }"
-          >
-            <span 
-              class="flex w-full"
-              :class="{
-                'animate-bounce': category[0] === 'priority'
-              }"
-            >
-              <div class="flex basis-5 pr-1 justify-center items-center">
-                <font-awesome-icon
-                  v-if="category[0] === 'priority'"
-                  :icon="['fas', 'heart']"
-                />
-                <font-awesome-icon
-                  v-else-if="category[0] === 'content'"
-                  :icon="['fas', 'video']"
-                />
-                <font-awesome-icon
-                  v-else-if="category[0] === 'social'"
-                  :icon="['fas', 'user-group']"
-                />
-                <font-awesome-icon
-                  v-else
-                  :icon="['fas', 'question']"
-                />
-              </div>
-              {{ !(category[0] === 'priority') ? category[0] : "Floppy's picks" }}
-            </span>
-          </div>
-        </div>
-        <div 
-          class="flex"
+  <div class="flex flex-col grow">
+      <div class="md:hidden pt-20"></div>
+      <div class="group flex flex-col-reverse grow gap-2">
+        <div
+          v-for="category in categories" 
+          :key="category[0]"
+          class="group/category flex flex-col grow py-1 pl-2 pr-1"
           :class="{
-            'hidden': collapseCategory[category[0]]
+            'mb-4': category[0] === 'priority',
+            'backdrop-opacity-90 backdrop-saturate-200 backdrop-brightness-50 backdrop-blur-sm shadow-lg shadow-black/50': !collapseCategory[category[0]]
           }"
         >
-          <div class="basis-4 shrink-0 mt-1 mb-1 border border-t-0 border-r-0 border-red-500 opacity-50"></div>
-          <div class="basis-full">
+          <div 
+            class="flex cursor-pointer"
+            @click="collapse((category[0] as Categories))"
+          >
+            <div class="flex basis-4 shrink-0 opacity-50 justify-center items-center hover:opacity-100 transition-opacity">
+              <font-awesome-icon 
+                class="fa-1x transition-transform duration-100"
+                :class="{
+                  '-rotate-90': collapseCategory[category[0]]
+                }"
+                :icon="[
+                  'fas',
+                  'chevron-down'
+                ]"
+              />
+            </div>
             <div 
-              class="flex flex-wrap-reverse flex-row-reverse text-center m-1"
+              class="flex basis-full mx-2 border-dotted border-red-500 font-bold text-sm capitalize opacity-75 group-hover/category:opacity-100 transition-opacity"
               :class="{
-                'animate-[bgpulse_5s_ease-in-out_infinite]': category[0] === 'priority'
+                'border-b': !collapseCategory[category[0]]
               }"
             >
-              <div
-                v-for="link in category[1].slice().reverse()"
-                :key="link.href"
-                class="basis-1/2 md:basis-1/3 grow shrink-0 xl:basis-1/4 m-1"
+              <span 
+                class="flex w-full"
+                :class="{
+                  'animate-bounce': category[0] === 'priority'
+                }"
               >
-                <ElementLink
-                  class="min-w-fit max-h-10 opacity-100 group-hover:opacity-60 hover:!opacity-100"
-                  :link="link"
-                  @click="$emit('click', link.href)"
-                />
+                <div class="flex basis-5 pr-1 justify-center items-center">
+                  <font-awesome-icon
+                    v-if="category[0] === 'priority'"
+                    :icon="['fas', 'heart']"
+                  />
+                  <font-awesome-icon
+                    v-else-if="category[0] === 'content'"
+                    :icon="['fas', 'video']"
+                  />
+                  <font-awesome-icon
+                    v-else-if="category[0] === 'social'"
+                    :icon="['fas', 'user-group']"
+                  />
+                  <font-awesome-icon
+                    v-else
+                    :icon="['fas', 'question']"
+                  />
+                </div>
+                {{ !(category[0] === 'priority') ? category[0] : "Floppy's picks" }}
+              </span>
+            </div>
+          </div>
+          <div 
+            class="flex"
+            :class="{
+              'hidden': collapseCategory[category[0]]
+            }"
+          >
+            <div class="basis-4 shrink-0 mt-1 mb-1 border border-t-0 border-r-0 border-red-500 opacity-50"></div>
+            <div class="basis-full">
+              <div 
+                class="flex flex-wrap-reverse flex-row-reverse text-center m-1"
+                :class="{
+                  'animate-[bgpulse_5s_ease-in-out_infinite]': category[0] === 'priority'
+                }"
+              >
+                <div
+                  v-for="link in category[1].slice().reverse()"
+                  :key="link.href"
+                  class="basis-1/2 md:basis-1/3 grow shrink-0 xl:basis-1/4 m-1"
+                >
+                  <ElementLink
+                    class="min-w-fit max-h-10 opacity-100 group-hover:opacity-60 hover:!opacity-100"
+                    :link="link"
+                    @click="$emit('click', link.href)"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="md:hidden pt-20"></div>
+      <div class="md:hidden pt-6"></div>
     </div>
   </div>
 </template>
