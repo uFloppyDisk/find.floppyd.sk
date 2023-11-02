7a6691bd0ed53420e6eee8dd9f248eb52e3d7784
uFloppyDisk <21136755+uFloppyDisk@users.noreply.github.com>
2023-11-01 19:11:01 -0700

    Moved shell logic to base Shell component;

--- components/Element/ShellInput.vue
+++ components/Element/ShellInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="shell" tabindex="0">
+  <div>
     <span class="text-red-500/80">
       <span>{{ $parent?.userName ?? 'unknown' }}</span>
       <span>@localhost: ~</span>
@@ -7,7 +7,7 @@
       <span>$ </span> 
     </span>
     <span>
-      <span ref="input" class="inline-flex basis-0 grow shrink bg-transparent" role="textbox"></span>
+      <span class="inline-flex basis-0 grow shrink bg-transparent" role="textbox">{{ input }}</span>
       <span class="absolute animate-pulse -translate-x-0.5">
         <font-awesome-icon 
           class="scale-x-[.60] scale-y-95"
@@ -19,29 +19,9 @@
 </template>
 
 <script lang="ts" setup>
-const shell: Ref<HTMLSpanElement | null> = ref(null);
-const input: Ref<HTMLSpanElement | null> = ref(null);
-
-if (process.browser) {
-  watch(shell, (value, _) => {
-    if (value == null) { return; }
-
-    value.addEventListener("keyup", (event) => {
-      if (input.value == null) { return; }
-
-      const text = input.value.innerText;
-
-      switch (event.key) {
-        case 'Backspace':
-          input.value.innerText = text.slice(0, -1);
-          break;
-
-        default:
-          input.value.innerText += event.key;
-      }
-    });
-  }) 
-}
+defineProps<{
+  input: String;
+}>();
 </script>
 
 <style scoped>
--- components/Shell.vue
+++ components/Shell.vue
@@ -1,26 +1,83 @@
 <template>
-  <div class="absolute top-full">
-    <ElementShellHistory>find -name "FloppyDisk" -type gamer -not cringe</ElementShellHistory>
-    <ElementShellInput />
+  <div ref="shell" class="absolute top-full w-full h-screen" tabindex="0">
+    <ElementShellHistory v-for="line in previous">{{ line }}</ElementShellHistory>
+    <ElementShellInput :input="input" @commit="commit" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { randomString } from '~/src/utils';
 
+const shell: Ref<HTMLSpanElement | null> = ref(null);
+const input: Ref<String> = ref('');
+
 const userName = ref('');
 const path = ref('');
+
+const history: String[] = [];
+const previous: String[] = reactive(['find -name "FloppyDisk" -type gamer -not cringe'])
 
 defineExpose<{
   userName: Ref<String>;
   path: Ref<String>;
 }>({userName, path});
 
+const commit = (text: String) => {
+  history.push(text);
+
+  if (text === 'clear') {
+    previous.splice(0, previous.length)
+    return;
+  }
+
+  previous.push(text);
+}
+
+const keyDownEvent = (event: KeyboardEvent) => {
+  if (input.value == null) { return; }
+
+  if (event.key.length > 1) {
+    switch (event.key) {
+      case 'Enter':
+        commit(input.value);
+        input.value = '';
+        break;
+
+      case 'Backspace':
+        if (input.value.length <= 0) { break; }
+
+        input.value = input.value.slice(0, -1);
+        break;
+    }
+
+    return;
+  }
+
+  if (event.ctrlKey || event.altKey) { 
+    if (event.ctrlKey && event.key === 'a') {
+      event.preventDefault();
+    }
+
+    return;
+  }
+
+  const charCode = event.key.charCodeAt(0);
+  if ((charCode >= 33 && charCode <= 126)) {
+    input.value += event.key;
+  }
+}
+
 if (process.browser) {
   window.addEventListener('DOMContentLoaded', () => {
     userName.value = `user-${randomString()}`;
     path.value = window.location.pathname;
-  })
+  });
+
+  watch(shell, (value, _) => {
+    if (value == null) { return; }
+
+    value.addEventListener("keydown", keyDownEvent);
+  });
 }
 
 </script>
