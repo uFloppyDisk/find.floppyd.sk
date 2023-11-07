7aa0c6e622a3414e3524bfb6314e16fdbac51224
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2023-11-06 17:47:49 -0800

    Regenerated twitch-get-status using CF wrangler;

--- packages/workers/twitch-get-status/__tests__/twitch-get-status.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const twitchGetStatus = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(twitchGetStatus(), 'Hello from twitchGetStatus');
-console.info('twitchGetStatus tests passed');
--- packages/workers/twitch-get-status/lib/twitch-get-status.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = twitchGetStatus;
-
-function twitchGetStatus() {
-  return 'Hello from twitchGetStatus';
-}
