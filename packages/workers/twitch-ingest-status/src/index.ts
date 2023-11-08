ed168a57bb745746db571e546ad680fae266722d
uFloppyDisk <21136755+uFloppyDisk@users.noreply.github.com>
2023-11-08 00:16:59 -0800

    Added twitch ingest status logic;

--- packages/workers/twitch-ingest-status/src/index.ts
+++ packages/workers/twitch-ingest-status/src/index.ts
@@ -8,9 +8,53 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export type TwitchEventSubStatusEnabled = {
+	status: 'enabled'
+}
+
+export type TwitchEventSubStatusVerification = {
+	status: 'webhook_callback_verification_pending'
+}
+
+export type TwitchEventSubBody = {
+	id: string,
+	version: string,
+	condition: { broadcaster_user_id: string },
+	transport: { method: 'webhook', callback: string },
+	created_at: string,
+	cost: number
+} & TwitchEventSubStatusEnabled | TwitchEventSubStatusVerification;
+
+export type TwitchEventStreamOnline = {
+	subscription: TwitchEventSubBody & {
+		type: 'stream.online',
+	},
+	event: {
+		id: string,
+		broadcaster_user_id: string,
+		broadcaster_user_login: string,
+		broadcaster_user_name: string,
+		type: 'live' | 'playlist' | 'watch_party' | 'premiere' | 'rerun',
+		started_at: string
+	}
+}
+
+export type TwitchEventStreamOffline = {
+	subscription: TwitchEventSubBody & {
+		type: 'stream.offline',
+	},
+	event: {
+		broadcaster_user_id: string,
+		broadcaster_user_login: string,
+		broadcaster_user_name: string
+	}
+}
+
+export type TwitchEvent = TwitchEventStreamOnline | TwitchEventStreamOffline;
+
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	FD_CONTENT_STATUS: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -27,6 +71,40 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const SUBSCRIPTIONS = ['stream.online', 'stream.offline'];
+
+		if (!(request.method.toUpperCase() === 'POST')) { return new Response(''); }
+		
+		const data: TwitchEvent = JSON.parse(await request.text());
+		if (data.subscription.status === 'webhook_callback_verification_pending') {
+			return new Response((data as TwitchEvent & { challenge: string }).challenge);
+		}
+
+		if (!(SUBSCRIPTIONS.includes(data.subscription.type))) { return new Response(''); }
+
+		switch (data.subscription.type) {
+			case "stream.online":
+				if (['playlist', 'rerun'].includes((data as TwitchEventStreamOnline).event.type)) { break;}
+
+				await env.FD_CONTENT_STATUS.put("twitch", JSON.stringify({
+					live: true,
+					started_at: (data as TwitchEventStreamOnline).event.started_at
+				}))
+
+				break;
+
+			case "stream.offline":
+				await env.FD_CONTENT_STATUS.put("twitch", JSON.stringify({
+					live: false,
+					started_at: ''
+				}))
+
+				break;
+		
+			default:
+				break;
+		}
+
+		return new Response('');
 	},
 };
