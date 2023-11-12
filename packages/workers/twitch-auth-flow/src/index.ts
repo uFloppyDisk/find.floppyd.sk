0107bbe0a512c6618ca1506935452df7fa282d9c
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2023-11-11 17:18:30 -0800

    Added temp auth logic in twitch-auth-flow;

--- packages/workers/twitch-auth-flow/src/index.ts
+++ packages/workers/twitch-auth-flow/src/index.ts
@@ -14,7 +14,7 @@
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	// FD_CONTENT_AUTH: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -33,18 +33,20 @@ export interface Env {
 }
 
 export default {
-	// The scheduled handler is invoked at the interval set in our wrangler.toml's
-	// [[triggers]] configuration.
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+		const tokenRequest = {
+			body: JSON.stringify({
+				client_id: env.TWITCH_CLIENT_ID,
+				client_secret: env.TWITCH_CLIENT_SECRET,
+				grant_type: "client_credentials"
+			}),
+			method: "POST",
+			headers: {
+				"content-type": "application/json;charset=UTF-8",
+			}
+		}
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		const resp = await fetch('https://id.twitch.tv/oauth2/token', tokenRequest);
+		console.log(resp.ok ? 'success' : 'fail');
 	},
 };
