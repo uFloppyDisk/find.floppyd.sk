9e519afc2ab15c94a4c855177c45fc458b626a87
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2023-11-16 16:46:32 -0800

    Repurpose twitch-auth-flow to handle subscribing to events;

--- packages/workers/twitch-auth-flow/src/index.ts
+++ packages/workers/twitch-auth-flow/src/index.ts
@@ -1,52 +1,48 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// FD_CONTENT_AUTH: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-	//
-	// Example binding to a D1 Database. Learn more at https://developers.cloudflare.com/workers/platform/bindings/#d1-database-bindings
-	// DB: D1Database
+	FD_CONTENT_SECRETS: KVNamespace;
 }
+
+const URI_TWITCH_EVENTSUB = 'https://api.twitch.tv/helix/eventsub/subscriptions'
+
+const HEADERS_SUBSCRIBE = {
+	'Content-Type': 'application/json'
+}
+
+const TWITCH_USER_ID = 37705020;
+
+const SUBSCRIPTIONS = [
+	{
+		type: "stream.online",
+		condition: {
+			"broadcaster_user_id": TWITCH_USER_ID
+		}
+	},
+	{
+		type: "stream.offline",
+		condition: {
+			"broadcaster_user_id": TWITCH_USER_ID
+		}
+	}
+]
 
 export default {
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		const tokenRequest = {
-			body: JSON.stringify({
-				client_id: env.TWITCH_CLIENT_ID,
-				client_secret: env.TWITCH_CLIENT_SECRET,
-				grant_type: "client_credentials"
-			}),
-			method: "POST",
-			headers: {
-				"content-type": "application/json;charset=UTF-8",
-			}
-		}
+		console.log(`Ran at ${event.cron}`);
 
-		const resp = await fetch('https://id.twitch.tv/oauth2/token', tokenRequest);
-		console.log(resp.ok ? 'success' : 'fail');
+		for (const sub of SUBSCRIPTIONS) {
+			const body = {
+				"version": "2",
+				"type": sub.type,
+				"condition": sub.condition,
+			}
+
+			await fetch(
+				URI_TWITCH_EVENTSUB,
+				{
+					headers: HEADERS_SUBSCRIBE,
+					body: JSON.stringify(body)
+				}
+			)
+		}
 	},
 };
