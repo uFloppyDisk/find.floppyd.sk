8bd1407ac153079493b8cb4c3d682f3706bca2b7
uFloppyDisk <21136755+uFloppyDisk@users.noreply.github.com>
2023-12-05 00:50:21 -0800

    Added hosting details section and referral;

--- packages/site/pages/about.vue
+++ packages/site/pages/about.vue
@@ -2,7 +2,7 @@
   <div class="flex-col gap-4">
     <ElementHeading :cursor="true">About Me</ElementHeading>
     <div>
-      <ElementHeading :heading="2">Who am I?</ElementHeading>
+      <ElementHeading :heading="2">Who are you?</ElementHeading>
       <div class="flex flex-row">
         <div class="basis-6 shrink-0"></div>
         <div class="flex flex-col gap-2">
@@ -29,9 +29,42 @@
         <div class="flex flex-col gap-2">
           <p>
             This website is a personal project of mine;
-            it is intended as a quasi-ARG (alternate reality game) in the form
-            of a terminal like you would see when using Linux or WSL.
+            a custom-made link aggregator that is also intended as a 
+            quasi-ARG (alternate reality game) in the form of a 
+            terminal like you would see when using Linux or WSL.
             I am always adding to this site so be sure to check back! 
+          </p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <ElementHeading :heading="2">How is this site hosted?</ElementHeading>
+      <div class="flex flex-row">
+        <div class="basis-6 shrink-0"></div>
+        <div class="flex flex-col gap-2">
+          <p>
+            I am hosting this site using DigitalOcean's App Platform with APIs
+            running on the edge using Cloudflare Workers and Workers KV. And the best part: it
+            is completely free!
+          </p>
+          <div class="flex w-full gap-2">
+            <ElementButton class="w-auto h-10 px-1" @click="goToLink('https://m.do.co/c/c002220143af')">
+              <font-awesome-icon
+                class="fa-2x scale-75"
+                :icon="[
+                  'fab',
+                  'digital-ocean'
+                ]"
+              />
+              <div class="ml-1 text-sm">
+                <div>Powered by <span class="font-bold">DigitalOcean</span></div>
+                <div class="text-xs">Get $200 dollars to start!</div>
+              </div>
+            </ElementButton>
+          </div>
+          <p>
+            To see the source code for this site, click the 'maximize' button (box icon)
+            in the top-right of the terminal window.
           </p>
         </div>
       </div>
@@ -40,5 +73,7 @@
 </template>
 
 <script setup lang="ts">
-
+const goToLink = (href: string) => {
+  window.open(href, '_blank', 'noopener')
+}
 </script>
