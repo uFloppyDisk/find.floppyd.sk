6ff8b0388a4808c4a856d4b8ff1093b154585cb2
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2023-12-21 19:23:14 -0800

    Fixed indenting level;

--- packages/site/src/commands.ts
+++ packages/site/src/commands.ts
@@ -1,104 +1,107 @@
 import type { ShellContext } from './types/shell.d.ts';
 import type { CommandMap } from './types/command.d.ts';
 import {
-    NullCommand,
-    Command,
-    CommandWithInput,
-    CommandWithoutPush
+  NullCommand,
+  Command,
+  CommandWithInput,
+  CommandWithoutPush
 } from './classes/command.ts';
 import colors from 'tailwindcss/colors';
 
 export function getCommand(commands: CommandMap, keyword: string): typeof Command | typeof NullCommand {
-    const def = commands.get(keyword);
-    if (typeof def == "undefined") { return NullCommand; }
+  const def = commands.get(keyword);
+  if (typeof def == "undefined") { return NullCommand; }
 
-    return def;
+  return def;
 }
 
 export function initCommand(def: typeof Command, input: string[]): Command {
-    if (def.prototype instanceof CommandWithInput) {
-        return new (def as typeof CommandWithInput)(input);
-    }
+  if (def.prototype instanceof CommandWithInput) {
+    return new (def as typeof CommandWithInput)(input);
+  }
 
-    return new def();
+  return new def();
 }
 
 export default <CommandMap> new Map([
-    ["clear", class ClearCommand extends CommandWithoutPush {
-        keyword = "clear";
-        description = "Clear shell of all previously ran commands and output.";
-        execute(ctx: ShellContext): string | null {
-            ctx.previous.splice(0, ctx.previous.length)
-            return null;
+  ["clear", class ClearCommand extends CommandWithoutPush {
+    keyword = "clear";
+    description = "Clear shell of all previously ran commands and output.";
+    execute(ctx: ShellContext): string | null {
+      ctx.previous.splice(0, ctx.previous.length)
+      return null;
+    }
+  }],
+  ["help", class HelpCommand extends CommandWithInput {
+    keyword = "help";
+    description = `Display available commands or 
+    explain usage of other commands in detail.`;
+    execute(ctx: ShellContext): string | null {
+      if ((this.input?.length ?? 0) <= 1) {
+        return `Commands:\n${[...ctx.commands.keys()].sort().join(" ")}`;
+      }
+
+      const keyword = this.input?.at(1) ?? "";
+      const def = getCommand(ctx.commands, keyword);
+      if (def === NullCommand) {
+        return `${keyword}: command not found`;
+      }
+
+      const command = initCommand((def as typeof Command), []);
+
+      return `${keyword}: ${command.help()}`;
+    }
+  }],
+  ["echo", class EchoCommand extends CommandWithInput {
+    keyword = "echo";
+    description = "Print input to screen.";
+    execute(_ctx: ShellContext): string | null {
+      return this.input?.join(" ") ?? null;
+    }
+  }],
+  ["colour", class ColourCommand extends CommandWithInput {
+    keyword = "colour";
+    description = "Change the colour of the terminal.";
+    availableColours = Object.keys(colors).splice(5, 22);
+    help(): string {
+      return `${this.description}\n
+        Available colours: ${this.availableColours.join(", ")}
+      `;
+    };
+    execute(ctx: ShellContext): string | null {
+      let selected = 'red'; 
+
+      if (!this.input) { return null; }
+
+      do {
+        if (this.input.length <= 1) {
+          selected = this.availableColours[
+            Math.floor(Math.random() * this.availableColours.length)
+          ];
+
+          break;
         }
-    }],
-    ["help", class HelpCommand extends CommandWithInput {
-        keyword = "help";
-        description = `Display available commands or 
-        explain usage of other commands in detail.`;
-        execute(ctx: ShellContext): string | null {
-            if ((this.input?.length ?? 0) <= 1) {
-                return `Commands:\n${[...ctx.commands.keys()].sort().join(" ")}`;
-            }
 
-            const keyword = this.input?.at(1) ?? "";
-            const def = getCommand(ctx.commands, keyword);
-            if (def === NullCommand) {
-                return `${keyword}: command not found`;
-            }
-
-            const command = initCommand((def as typeof Command), []);
-
-            return `${keyword}: ${command.help()}`;
+        if (!this.availableColours.includes(this.input[1])) {
+          return `
+            Colour '${this.input[1]}' does not exist. 
+            Type 'help ${this.keyword}' to see a list of available colours.
+          `
         }
-    }],
-    ["echo", class EchoCommand extends CommandWithInput {
-        keyword = "echo";
-        description = "Print input to screen.";
-        execute(_ctx: ShellContext): string | null {
-            return this.input?.join(" ") ?? null;
-        }
-    }],
-    ["colour", class ColourCommand extends CommandWithInput {
-        keyword = "colour";
-        description = "Change the colour of the terminal.";
-        availableColours = Object.keys(colors).splice(5, 22);
-        help(): string {
-          return `${this.description}\n
-          Available colours: ${this.availableColours.join(", ")}`;
-        };
-        execute(ctx: ShellContext): string | null {
-          let selected = 'red'; 
 
-          if (!this.input) { return null; }
-
-          do {
-            if (this.input.length <= 1) {
-              selected = this.availableColours[
-                Math.floor(Math.random() * this.availableColours.length)
-              ];
-
-              break;
-            }
-
-            if (!this.availableColours.includes(this.input[1])) {
-              return `Colour '${this.input[1]}' does not exist. 
-                Type 'help ${this.keyword}' to see a list of available colours.`
-            }
-            
-            selected = this.input[1];
-          } while (false);
+        selected = this.input[1];
+      } while (false);
 
 
-          const grades = colors[selected];
-          for (const grade of Object.keys(grades)) {
-            ctx.root?.documentElement.style.setProperty(
-              `--color-primary-${grade}`, grades[grade]
-            )
-            
-          }
+      const grades = colors[selected];
+      for (const grade of Object.keys(grades)) {
+        ctx.root?.documentElement.style.setProperty(
+          `--color-primary-${grade}`, grades[grade]
+        )
 
-          return `Changing colour to '${selected}'`;
-        }
-    }],
+      }
+
+      return `Changing colour to '${selected}'`;
+    }
+  }],
 ]);
