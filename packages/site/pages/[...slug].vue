d05015d5ef5abe0123add7a69dbbdd4a7c69de6e
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2023-12-31 02:49:12 -0800

    Use Category component; Separate priority category from others;

--- packages/site/pages/[...slug].vue
+++ packages/site/pages/[...slug].vue
@@ -1,104 +1,35 @@
 <template>
   <div class="flex-col">
     <ElementHeading :cursor="true">Find me</ElementHeading>
-    <div class="flex flex-col-reverse xl:flex-row-reverse xl:flex-wrap-reverse gap-y-2 select-none">
-      <div
-        v-for="category in categories" 
-        :key="category[0]"
-        class="group/category flex flex-col xl:basis-1/2 grow"
-        :class="{
-          'pb-2 xl:pb-0 border-b xl:border-b-0 border-primary-500 border-dashed': 
-            category[0] === 'priority',
-        }"
-      >
-        <div 
-          class="flex cursor-pointer"
-          @click="collapse((category[0] as Categories))"
-        >
-          <div class="flex basis-4 shrink-0 opacity-50 justify-center items-center hover:opacity-100 transition-opacity">
-            <font-awesome-icon 
-              class="fa-1x transition-transform duration-100"
-              :class="{
-                '-rotate-90': categoryCollapsed[category[0]]
-              }"
-              :icon="[
-                'fas',
-                'chevron-down'
-              ]"
-            />
-          </div>
-          <div 
-            class="flex basis-full mx-2 border-dotted border-primary-500 font-bold text-sm capitalize opacity-75 group-hover/category:opacity-100 transition-opacity"
-            :class="{
-              'border-b': !categoryCollapsed[category[0]]
-            }"
-          >
-            <span 
-              class="inline-flex w-full py-1 items-center"
-              :class="{
-                'animate-bounce': category[0] === 'priority' && categoryCollapsed[category[0]]
-              }"
-            >
-              <span class="flex basis-5 justify-center items-center">
-                <font-awesome-icon
-                  v-if="category[0] === 'priority'"
-                  :icon="['fas', 'heart']"
-                />
-                <font-awesome-icon
-                  v-else-if="category[0] === 'content'"
-                  :icon="['fas', 'video']"
-                />
-                <font-awesome-icon
-                  v-else-if="category[0] === 'social'"
-                  :icon="['fas', 'user-group']"
-                />
-                <font-awesome-icon
-                  v-else
-                  :icon="['fas', 'question']"
-                />
-              </span>
-              <span class="h-4">
-                {{ !(category[0] === 'priority') ? category[0] : "Floppy's picks" }}
-              </span>
-            </span>
-          </div>
-        </div>
-        <div 
-          class="flex"
-          :class="{
-            'hidden': categoryCollapsed[category[0]]
-          }"
-        >
-          <div class="basis-1 shrink-0"></div>
-          <div class="basis-3 shrink-0 mt-1 mb-1 border border-t-0 border-r-0 border-primary-500 opacity-50"></div>
-          <div class="basis-full">
-            <div 
-              class="flex flex-wrap-reverse flex-row-reverse text-center m-1"
-              :class="{
-                'animate-[bgpulse_5s_ease-in-out_infinite]': category[0] === 'priority'
-              }"
-            >
-              <div
-                v-for="link in category[1].slice().reverse()"
-                :key="link.href"
-                class="basis-full md:basis-1/2 lg:basis-1/3 xl:basis-full grow shrink-0"
-              >
-                <ElementLink
-                  class="min-w-fit max-h-10 m-1 opacity-100 group-hover:opacity-75 hover:!opacity-100"
-                  :link="link"
-                  @click="goToLink(link.href)"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="flex flex-col-reverse">
+      <div class="
+        flex pb-2 xl:pb-0 
+        border-b xl:border-b-0 border-primary-500 border-dashed 
+        order-last
+      ">
+        <ElementCategory 
+          v-if="categories.has('priority')"
+          :category="['priority', (categories.get('priority') as Link[])]"
+          class="basis-full xl:basis-1/2"
+        />
+      </div>
+      <div class="
+        flex flex-col-reverse 
+        xl:flex-row-reverse xl:flex-wrap-reverse
+        gap-y-2 select-none
+      ">
+        <ElementCategory 
+          v-for="category in categoriesMinusPriority"
+          :key="category[0]"
+          :category="category"
+          class="xl:basis-1/3 grow"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { Categories } from '~/src/categories';
 import type { Link } from '~/src/links';
 
 const route = useRoute();
@@ -108,24 +39,23 @@ type CategoryMap = Map<string, Link[]>;
 
 const allCategories: Ref<CategoryMap> = ref(inject("categories") ?? new Map());
 const categories: Ref<CategoryMap> = ref(new Map());
-const categoryCollapsed: Ref<Record<string, boolean>> = ref({});
+const categoriesMinusPriority: Ref<CategoryMap> = ref(new Map());
 
-const collapse = (category: Categories) => {
-  categoryCollapsed.value[category] = !categoryCollapsed.value[category];
-}
+do {
+  if (typeof select === 'undefined' || select.length <= 0) {
+    categories.value = allCategories.value;
+    break;
+  }
 
-const goToLink = (href: string) => {
-  window.open(href, '_blank', 'noopener')
-}
-
-if (typeof select !== 'undefined' && select.length > 0) {
   const selectAdd = ['priority', ...select];
   categories.value = new Map([...allCategories.value]
                         .filter(([k, _v]) => selectAdd.includes(k)));
-} else {
-  categories.value = allCategories.value;
-}
+} while (false);
 
+categoriesMinusPriority.value = new Map(
+  [...categories.value.entries()]
+  .filter((cat) => cat[0] !== 'priority')
+);
 </script>
 
 <style>
