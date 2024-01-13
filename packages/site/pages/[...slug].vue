6a48a71a2ccbe7c345717ece2795ee9b3361c2ad
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2024-01-12 18:44:49 -0800

    Improved category layout on xl displays; Revert separation of priority category;

--- packages/site/pages/[...slug].vue
+++ packages/site/pages/[...slug].vue
@@ -1,30 +1,21 @@
 <template>
   <div class="flex-col">
     <ElementHeading :cursor="true">Find me</ElementHeading>
-    <div class="flex flex-col-reverse">
-      <div class="
-        flex pb-2 xl:pb-0 
-        border-b xl:border-b-0 border-primary-500 border-dashed 
-        order-last
-      ">
-        <ElementCategory 
-          v-if="categories.has('priority')"
-          :category="['priority', (categories.get('priority') as Link[])]"
-          class="basis-full xl:basis-1/2"
-        />
-      </div>
-      <div class="
-        flex flex-col-reverse 
-        xl:flex-row-reverse xl:flex-wrap-reverse
-        gap-y-2 select-none
-      ">
-        <ElementCategory 
-          v-for="category in categoriesMinusPriority"
-          :key="category[0]"
-          :category="category"
-          class="xl:basis-1/3 grow"
-        />
-      </div>
+    <div class="
+      flex flex-col-reverse 
+      xl:flex-row-reverse xl:flex-wrap-reverse 
+      gap-y-2 select-none
+    ">
+      <div class="col-span-2" />
+      <ElementCategory 
+        v-for="category in categories"
+        :key="category[0]"
+        :category="category"
+        class="xl:basis-1/4"
+        :class="{
+          'pb-2 xl:pb-0 border-b xl:border-b-0 border-primary-500 border-dashed xl:grow': category[0] === 'priority'
+        }"
+      />
     </div>
   </div>
 </template>
@@ -39,7 +30,6 @@ type CategoryMap = Map<string, Link[]>;
 
 const allCategories: Ref<CategoryMap> = ref(inject("categories") ?? new Map());
 const categories: Ref<CategoryMap> = ref(new Map());
-const categoriesMinusPriority: Ref<CategoryMap> = ref(new Map());
 
 do {
   if (typeof select === 'undefined' || select.length <= 0) {
@@ -51,11 +41,6 @@ do {
   categories.value = new Map([...allCategories.value]
                         .filter(([k, _v]) => selectAdd.includes(k)));
 } while (false);
-
-categoriesMinusPriority.value = new Map(
-  [...categories.value.entries()]
-  .filter((cat) => cat[0] !== 'priority')
-);
 </script>
 
 <style>
