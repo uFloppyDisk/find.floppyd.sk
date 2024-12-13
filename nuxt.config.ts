835715dd57cdfdad8da40f91506338775725a0dd
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2024-12-13 15:41:39 -0800

    chore!: move site to project root;

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+      public: {
+          CFWorkerTwitchGetStatus: process.env.CF_WORKER_TWITCH_GET_STATUS
+      }
+  },
+
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+
+  build: {
+      transpile: [
+          '@fortawesome/vue-fontawesome',
+          '@fortawesome/fontawesome-svg-core',
+          '@fortawesome/free-solid-svg-icons',
+          '@fortawesome/free-regular-svg-icons',
+          '@fortawesome/free-brands-svg-icons',
+      ],
+  },
+
+  compatibilityDate: '2024-12-13',
+});
