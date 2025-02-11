6d667ac50e615edb6447f230ed31ca43bb0ee9a5
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-02-10 18:43:15 -0800

    style: formatting;

--- src/links.ts
+++ src/links.ts
@@ -1,105 +1,105 @@
 import type { Link } from "../types/link.d.ts";
 
 export default <Link[]>[
-    {
-        platform: 'site',
-        href: 'https://floppydiskprod.com',
+  {
+    platform: 'site',
+    href: 'https://floppydiskprod.com',
 
-        title: 'FloppyDiskProd',
-        subtitle:
-            'Business-oriented and client-facing portfolio and marketplace.',
+    title: 'FloppyDiskProd',
+    subtitle:
+      'Business-oriented and client-facing portfolio and marketplace.',
 
-        category: 'software',
-    },
-    {
-        platform: 'site',
-        href: 'https://io.floppyd.sk',
+    category: 'software',
+  },
+  {
+    platform: 'site',
+    href: 'https://io.floppyd.sk',
 
-        title: 'FD I/O',
-        subtitle:
-            'A custom-built web platform for hosting my own web applications.',
+    title: 'FD I/O',
+    subtitle:
+      'A custom-built web platform for hosting my own web applications.',
 
-        category: 'software',
-    },
-    {
-        platform: 'youtube',
-        href: 'https://floppyd.sk/yt-prod',
+    category: 'software',
+  },
+  {
+    platform: 'youtube',
+    href: 'https://floppyd.sk/yt-prod',
 
-        title: 'productions',
-        subtitle: 'Professional video productions for clients or for fun!',
+    title: 'productions',
+    subtitle: 'Professional video productions for clients or for fun!',
 
-        category: 'content',
-    },
-    {
-        platform: 'youtube',
-        href: 'https://floppyd.sk/yt-gaming',
+    category: 'content',
+  },
+  {
+    platform: 'youtube',
+    href: 'https://floppyd.sk/yt-gaming',
 
-        title: 'gaming',
-        subtitle: 'Laid-back videos with goofs and gaffs.',
+    title: 'gaming',
+    subtitle: 'Laid-back videos with goofs and gaffs.',
 
-        category: 'content',
-    },
-    {
-        platform: 'twitch',
-        href: 'https://floppyd.sk/twitch',
+    category: 'content',
+  },
+  {
+    platform: 'twitch',
+    href: 'https://floppyd.sk/twitch',
 
-        title: 'twitch',
+    title: 'twitch',
 
-        priority: true,
-        category: 'content',
-    },
-    {
-        platform: 'discord',
-        href: 'https://floppyd.sk/discord',
+    priority: true,
+    category: 'content',
+  },
+  {
+    platform: 'discord',
+    href: 'https://floppyd.sk/discord',
 
-        title: 'discord',
-        subtitle: 'Official unofficial FloppyDisk hangout.',
+    title: 'discord',
+    subtitle: 'Official unofficial FloppyDisk hangout.',
 
-        category: 'social',
-    },
-    {
-        platform: 'steam',
-        href: 'https://floppyd.sk/steam',
+    category: 'social',
+  },
+  {
+    platform: 'steam',
+    href: 'https://floppyd.sk/steam',
 
-        title: 'steam',
+    title: 'steam',
 
-        category: 'social',
-    },
-    {
-        platform: 'github',
-        href: 'https://floppyd.sk/github',
+    category: 'social',
+  },
+  {
+    platform: 'github',
+    href: 'https://floppyd.sk/github',
 
-        title: 'github',
-        subtitle: "See what I'm working on!",
+    title: 'github',
+    subtitle: "See what I'm working on!",
 
-        priority: true,
-        category: 'software',
-    },
-    {
-        platform: 'twitter',
-        href: 'https://floppyd.sk/twitter',
+    priority: true,
+    category: 'software',
+  },
+  {
+    platform: 'twitter',
+    href: 'https://floppyd.sk/twitter',
 
-        title: 'tweets',
+    title: 'tweets',
 
-        category: 'social',
-    },
-    {
-        platform: 'instagram',
-        href: 'https://floppyd.sk/instagram',
+    category: 'social',
+  },
+  {
+    platform: 'instagram',
+    href: 'https://floppyd.sk/instagram',
 
-        title: 'images',
+    title: 'images',
 
-        category: 'social',
-    },
-    {
-        platform: 'site',
-        href: 'https://emerldd.com',
+    category: 'social',
+  },
+  {
+    platform: 'site',
+    href: 'https://emerldd.com',
 
-        title: 'Emerldd.com',
-        subtitle: 
-            'Community platform custom-built for Emerldd, a Twitch- and YouTube-partnered content creator.',
+    title: 'Emerldd.com',
+    subtitle:
+      'Community platform custom-built for Emerldd, a Twitch- and YouTube-partnered content creator.',
 
-        priority: true,
-        category: 'software',
-    },
+    priority: true,
+    category: 'software',
+  },
 ];
